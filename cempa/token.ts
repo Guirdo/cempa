366428d7c68e1628c8971de833504ee523edc464
Guirdo <produccionesaldo@gmail.com>
2022-07-31 19:11:03 -0500

    Token types added

--- cempa/token.ts
+++ cempa/token.ts
@@ -1,8 +1,59 @@
 enum TokenType  {
-    AND,
-    ASSIGN,
+    //Delimeters
     COMMA,
+    SEMICOLON,
+    LPAREN,
+    RPAREN,
+    ENDLINE,
+    FIN,
+
+    //Aritmethics operators
+    PLUS,
+    MINUS,
+    MULTIPLICATION,
+    DIVISION,
+    MODULUS,
+    INCREMENT,
+    DECREMENT,
+
+    //Logic operators
+    AND,
+    OR,
+    LESSER,
+    GREATER,
+    EQUAL,
+    LESSER_EQ,
+    GREATER_EQ,
+    NEGATION,
+    UNEQUAL,
+
+    //Control
+    SI,
+    OSI,
+    SINO,
+    SEGUN,
+    CASO,
+    PORDEFECTO,
+
+    //Assignation
+    ASSIGN,
+
+    //CLASSES
     CLASSNAME,
+    FUNCTION,
+
+    //Literals
+    NUMBER,
+    STRING,
+    BOOLEAN,
+    COMMENT,
+
+    //Loops
+    PARA,
+    CADA,
+    HACER,
+    MIENTRAS,
+
 }
 
 type Token = {
